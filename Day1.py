8a31a2c938d53ebdf2fc5e8afdf59c6abfd4647f
Abul Hossain Khan <140191921+abulgit@users.noreply.github.com>
2023-08-24 22:57:28 +0530

    Day1.py

---
+++ Day1.py
@@ -0,0 +1,26 @@
+#Print Your Name
+print ('Abul Hossain Khan')
+
+#Simple Calculator
+a= int(input('Enter a Number'))
+b= int(input('Enter a Another Number'))
+c= a+b
+d= a-b
+e= a*b
+f= a/b
+print ('Addition is', c)
+print ('Subtraction is', d)
+print ('Multilication is', e)
+print ('Division is', f)
+
+#Print Reminder
+a= int(input('Enter a Number'))
+b= int(input('Enter a Number'))
+c= a%b
+print ('Reminder Is',c)
+
+  #concatenate Two Strings.
+a= input('Enter a Number')
+b= input('Enter a Another Number')
+c= a+b
+print ('Concatenate of this two number is',c)
